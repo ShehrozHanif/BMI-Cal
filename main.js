7c4e607e671b5e9a0bf4d066389bfdc38fb5d17f
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-21 10:16:06 -0700

    added main.ts and main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const height = await inquirer.prompt({
+    name: "user",
+    type: "number",
+    message: "Kindly Enter your height in meter?",
+});
+const weight = await inquirer.prompt({
+    name: "user",
+    type: "number",
+    message: "Kindly Enter your weight in Kg"
+});
+console.log(` Your BMI is ${weight.user / (height.user * height.user)} `);
